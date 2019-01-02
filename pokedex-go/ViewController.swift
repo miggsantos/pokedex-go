820a916550b2b9418f523c8eb0c62c26e07b8603
Miguel Santos <miggsantos@gmail.com>
2019-01-02 20:14:44 +0000

    Initial Commit

---
+++ pokedex-go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex-go
+//
+//  Created by Miguel Santos on 02/01/2019.
+//  Copyright © 2019 Miguel Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
